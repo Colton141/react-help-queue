a4df6ae2ae1970f239d074747a9d4f3a42990092
Colton Lacey and Marc Davies <marc@>
2019-07-01 11:22:27 -0700

    add babel and .gitignore

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 ReactDOM.render(
   <div>
     <h1>Help Queue</h1>
-    <h3>3a</h3>
+    <h3>1B</h3>
     <h3>Marc and Colton</h3>
     <p><em>Firebase will not save record!</em></p>
   </div>,
